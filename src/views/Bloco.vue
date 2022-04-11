2b54694073e286a90e856a34d22084c552b4f0f0
Aldir Lyra <aldir_lyra@hotmail.com>
2022-04-11 10:42:38 -0300

    Update Bloco.vue

--- src/views/Bloco.vue
+++ src/views/Bloco.vue
@@ -1,119 +1,166 @@
 <template>
-  <v-card>
-    <v-card-title>
-      Gerenciamento de Bloco
-      <v-spacer></v-spacer>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Buscar"
-        single-line
-        hide-details
-      ></v-text-field>
-    </v-card-title>
-    <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :search="search"
-    ></v-data-table>
-  </v-card>
+  <v-data-table
+    :headers="titulos"
+    :items="blocos"
+    :search="search"
+    class="elevation-2 data-table"
+  >
+    <template v-slot:top>
+      <v-toolbar flat>
+        <v-toolbar-title>Gerenciamento de Bloco</v-toolbar-title>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Pesquisar"
+          single-line
+          hide-details
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="400px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              small
+              class="mx-2 add"
+              fab
+              dark
+              color="green"
+              v-bind="attrs"
+              v-on="on"
+              ><v-icon dark> mdi-plus</v-icon></v-btn
+            >
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">{{ tituloForm }}</span>
+            </v-card-title>
+            <p v-if="errors.length">
+              <ul>
+                <li v-for="error in errors" :key="error">{{ error }}</li>
+             </ul>
+            </p>
+            <v-card-text>
+              <v-form>
+                <v-container>
+                  <v-row>
+                    <v-col cols="8" sm="6" md="4">
+                      <v-text-field
+                        v-model="itemAtual.campus"
+                        label="Campus"
+                      ></v-text-field>
+                    </v-col>
+                     <v-col cols="8" sm="6" md="4">
+                      <v-text-field
+                        v-model="itemAtual.sala"
+                        label="Sala"
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn small color="warning" dark @click="fechar">
+                Cancelar
+              </v-btn>
+              <v-btn 
+              small color="primary" 
+              class="mr-4"
+              @click="checkForm">Salvar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:[`item.acoes`]="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)" color="blue"> mdi-pencil </v-icon>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        search: '',
-        headers: [
-          {
-            text: 'Câmpus',
-            align: 'start',
-            sortable: false,
-            value: 'name',
-          },
-          {
-            text: 'Bloco',
-            align: 'start',
-            sortable: false,
-            value: 'name',
-          },
-          { text: 'Ações', value: 'fat' },
-        ],
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%',
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%',
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%',
-          },
-        ],
-      }
+export default {
+  data: () => ({
+    search: "",
+    dialog: false,
+    dialogDesativar: false,
+    titulos: [
+      {
+        text: "Câmpus",
+        value: "campus",
+        sortable: false,
+      },
+      {
+        text: "Sala",
+        value: "sala",
+        sortable: false,
+      },
+      {
+        text: "Ações",
+        value: "acoes",
+        sortable: false,
+      },
+    ],
+
+    errors: [],
+    blocos: [],
+    editIndice: -1,
+
+    itemAtual: {
+      id: null,
+      campus: "",
+      sala: "",
     },
-  }
+
+    itemPadrao: {
+      id: null,
+      campus: "",
+      sala: "",
+    },
+  }),
+
+  computed: {
+    tituloForm() {
+      return this.editIndice === -1 ? "Cadastrar Bloco" : "Editar Dados";
+    },
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.fechar();
+    },
+  },
+
+  editItem(item) {
+    this.editIndice = this.periodos.indexOf(item);
+    this.itemAtual = Object.assign({}, item);
+    this.dialog = true;
+  },
+
+  checkForm() {
+    if (this.itemAtual.campus && this.itemAtual.sala) {
+      this.salvar();
+      return true;
+    }
+
+    this.errors = [];
+
+    if (!this.itemAtual.campus) {
+      this.errors.push("O câmpus é obrigatório.");
+    }
+
+    if (!this.itemAtual.sala) {
+      this.errors.push("A sala é obrigatória.");
+    }
+  },
+
+  fechar() {
+    this.dialog = false;
+    this.$nextTick(() => {
+      this.itemAtual = Object.assign({}, this.itemPadrao);
+      this.editIndice = -1;
+    });
+  },
+};
 </script>
