d353f76fe27c47dfee4dd03f4db68c2edc7338ab
Aldir Lyra <aldir_lyra@hotmail.com>
2022-04-18 16:51:07 -0300

    logo unitins

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
-   <v-app id="inspire">
+  <v-app id="inspire">
     <v-navigation-drawer v-model="drawer" app color="#f1f1f1">
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="text-h6"> SIGHA </v-list-item-title>
-          <v-list-item-subtitle>
-            Sistema Gerencial 
-          </v-list-item-subtitle>
+          <v-list-item-subtitle> Sistema Gerencial </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
@@ -24,56 +22,62 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar app color="#f1f1f1">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
+    <v-app-bar app color="#f1f1f1">
       <v-app-bar-title
-        ></v-app-bar-title>
+        ><img src="../public/logo-teste.jpg" class="logo-unitins"
+      /></v-app-bar-title>
 
       <v-spacer></v-spacer>
       <!-- <img src="../assets/img/exit-to-aplicativo.png" class="sair" />
       <router-link :to="{ name: 'Login' }"></router-link> -->
       <v-btn @click="signout" v-if="btnlogout">sair</v-btn>
-    
     </v-app-bar>
 
     <v-main>
       <router-view />
     </v-main>
 
-    <v-footer
-    color="#104ADF"
-    padless
-  >
-    <v-row
-      justify="center"
-      no-gutters
-    >
-     
-      <v-col
-        class="py-4 text-center white--text"
-        cols="12"
-      >
-        {{ new Date().getFullYear() }} — <strong>SIGHA - Sistema de gerenciamento de horários acadêmicos</strong>
-      </v-col>
-    </v-row>
-  </v-footer>
+    <v-footer color="#104ADF" padless>
+      <v-row justify="center" no-gutters>
+        <v-col class="py-4 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} —
+          <strong
+            >SIGHA - Sistema de gerenciamento de horários acadêmicos</strong
+          >
+        </v-col>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 
+<style scoped>
+
+.logo-unitins{
+  padding-top: 2.2%;
+  width: 80%;
+  height: 80%;
+}
+
+</style>
+
 <script>
-  export default {
-    data: () => ({ 
-      drawer: null,
-      items: [
-          { title: 'Login',icon: "mdi-login",to:'/newlogin'},
-          { title: 'Semestres',icon: "mdi-sort-calendar-ascending", to:'/semestres'},
-          { title: 'Salas',icon: "mdi-google-classroom",to:'/salas'},
-          { title: 'Usuários', icon: "mdi-account-group", to:'/usuarios'},
-          { title: 'Bloco', icon: "mdi-lan", to:'/bloco'},
-          { title: 'Horários', icon: "mdi-book-clock", to:'/horarios'},
-          { title: 'Oferta', icon: "mdi-offer", to:'/oferta'},
-        ],
-    }),
-  }
+export default {
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Login", icon: "mdi-login", to: "/newlogin" },
+      {
+        title: "Semestres",
+        icon: "mdi-sort-calendar-ascending",
+        to: "/semestres",
+      },
+      { title: "Salas", icon: "mdi-google-classroom", to: "/salas" },
+      { title: "Usuários", icon: "mdi-account-group", to: "/usuarios" },
+      { title: "Bloco", icon: "mdi-lan", to: "/bloco" },
+      { title: "Horários", icon: "mdi-book-clock", to: "/horarios" },
+      { title: "Oferta", icon: "mdi-offer", to: "/oferta" },
+    ],
+  }),
+};
 </script>
