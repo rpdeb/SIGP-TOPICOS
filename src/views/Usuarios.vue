a02d415fee662a4eadbfb1869026d7b41e9eed75
rpdeb <deboraribeiro9015@gmail.com>
2022-05-22 23:58:55 -0300

    Update Usuarios.vue
    Método - Selecionar Perfil
    Ajuste do Form de Cadastro
    Ajuste no método de Salvar

--- src/views/Usuarios.vue
+++ src/views/Usuarios.vue
@@ -1,18 +1,38 @@
 <template>
-  <v-data-table :headers="titulos" :items="usuarios" :search="search" class="elevation-2 data-table" :footer-props="{
-    'items-per-page-text': 'Itens por página'
-  }">
+  <v-data-table
+    :headers="titulos"
+    :items="usuarios"
+    :search="search"
+    class="elevation-2 data-table"
+    :footer-props="{
+      'items-per-page-text': 'Itens por página',
+    }"
+  >
     <template v-slot:top>
       <v-toolbar flat>
         <v-toolbar-title>Gerenciamento de Usuários</v-toolbar-title>
         <v-divider class="mx-4" inset vertical></v-divider>
 
-        <v-text-field v-model="search" append-icon="mdi-magnify" label="Pesquisar" single-line hide-details>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Pesquisar"
+          single-line
+          hide-details
+        >
         </v-text-field>
         <v-spacer></v-spacer>
         <v-dialog v-model="dialog" max-width="400px">
           <template v-slot:activator="{ on, attrs }">
-            <v-btn small class="mx-2 add" fab dark color="green" v-bind="attrs" v-on="on">
+            <v-btn
+              small
+              class="mx-2 add"
+              fab
+              dark
+              color="green"
+              v-bind="attrs"
+              v-on="on"
+            >
               <v-icon dark> mdi-plus</v-icon>
             </v-btn>
           </template>
@@ -26,18 +46,39 @@
                 <v-container>
                   <v-row>
                     <v-col cols="8" sm="6" md="4">
-                      <v-select v-model="atributo.tipo" :items="items" :rules="[v => !!v || 'Item obrigatório!']"
-                        label="tipo" required></v-select>
+                      <v-text-field
+                        v-model="atributo.email"
+                        :rules="[(v) => !!v || 'Item obrigatório!']"
+                        label="Email"
+                        type="email"
+                        required
+                      ></v-text-field>
                     </v-col>
                     <v-col cols="8" sm="6" md="4">
-                      <v-text-field v-model="atributo.login" :items="items" :rules="[v => !!v || 'Item obrigatório!']"
-                        label="Login" required></v-text-field>
+                      <v-select
+                        v-model="atributo.tipo"
+                        @input="selecionarPerfil"
+                        :items="perfis"
+                        :rules="[(v) => !!v || 'Item obrigatório!']"
+                        label="Perfil"
+                        required
+                      ></v-select>
                     </v-col>
                     <v-col cols="8" sm="6" md="4">
-                      <v-select v-model="atributo.campus" :items="items" label="Campus"></v-select>
+                      <v-select
+                        v-model="atributo.campus"
+                        :items="arraycampus"
+                        item-value="id"
+                        label="Campus"
+                      ></v-select>
                     </v-col>
                     <v-col cols="8" sm="6" md="4">
-                      <v-select v-model="atributo.curso" :items="items" label="Curso"></v-select>
+                      <v-select
+                        v-model="atributo.curso"
+                        :items="arraycursos"
+                        item-value="id"
+                        label="Curso"
+                      ></v-select>
                     </v-col>
                   </v-row>
                 </v-container>
@@ -48,18 +89,30 @@
               <v-btn small color="warning" dark @click="fechar">
                 Cancelar
               </v-btn>
-              <v-btn small color="primary" class="mr-4" @click="salvar">Salvar</v-btn>
+              <v-btn small color="primary" class="mr-4" @click="salvar"
+                >Salvar</v-btn
+              >
             </v-card-actions>
           </v-card>
         </v-dialog>
 
         <v-dialog v-model="dialogDesativar" max-width="400px">
           <v-card>
-            <v-card-title class="text-h5">Deseja {{ mudarStatus }} este usuário? </v-card-title>
+            <v-card-title class="text-h5"
+              >Deseja {{ mudarStatus }} este usuário?
+            </v-card-title>
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn small color="warning" dark @click="dialogDesativar = false">Não</v-btn>
-              <v-btn small color="primary" class="mr-4" @click="desativeItemConfirm">Sim</v-btn>
+              <v-btn small color="warning" dark @click="dialogDesativar = false"
+                >Não</v-btn
+              >
+              <v-btn
+                small
+                color="primary"
+                class="mr-4"
+                @click="desativeItemConfirm"
+                >Sim</v-btn
+              >
               <v-spacer></v-spacer>
             </v-card-actions>
           </v-card>
@@ -67,8 +120,12 @@
       </v-toolbar>
     </template>
     <template v-slot:[`item.acoes`]="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)" color="blue"> mdi-pencil </v-icon>
-      <v-icon small class="mr-2" @click="desativeItem(item)" color="red"> mdi-power-standby </v-icon>
+      <v-icon small class="mr-2" @click="editItem(item)" color="blue">
+        mdi-pencil
+      </v-icon>
+      <v-icon small class="mr-2" @click="desativeItem(item)" color="red">
+        mdi-power-standby
+      </v-icon>
     </template>
   </v-data-table>
 </template>
@@ -101,23 +158,23 @@ export default {
     dialog: false,
     titulos: [
       {
-        text: "Tipo",
+        text: "Perfil",
         value: "tipo",
         sortable: false,
       },
       {
-        text: "Login",
-        value: "login",
+        text: "Email",
+        value: "email",
         sortable: false,
       },
       {
-        text: "Câmpus",
-        value: "campus",
+        text: "Campus",
+        value: "campus.label",
         sortable: false,
       },
       {
         text: "Curso",
-        value: "curso",
+        value: "curso.label",
         sortable: false,
       },
       {
@@ -127,26 +184,29 @@ export default {
       },
     ],
     usuarios: [],
-    tiposPerfil: [],
-    login: [],
-    campus: [],
-    curso: [],
+    perfis: ["Admin", "Gestão Administrativa", "Coordenador de Curso"],
+    perfilSelecionado: null,
+    idperfil: null,
+    arraycampus: [],
+    arraycursos: [],
+    cursosRaw: [],
+    campusRaw: [],
     editIndice: -1,
     atributo: {
       id: null,
-      tipo: "",
-      login: "",
-      campus: "",
-      curso: "",
+      tipo: null,
+      email: "",
+      campus: null,
+      curso: null,
       ativo: true,
     },
 
     atributoPadrao: {
       id: null,
-      tipo: "",
-      login: "",
-      campus: "",
-      curso: "",
+      tipo: null,
+      email: "",
+      campus: null,
+      curso: null,
       ativo: true,
     },
   }),
@@ -157,7 +217,7 @@ export default {
     },
     mudarStatus() {
       return this.atributo.ativo == "Ativo" ? "desativar " : "ativar ";
-    }
+    },
   },
 
   watch: {
@@ -168,31 +228,40 @@ export default {
 
   mounted() {
     this.inicializar();
+    this.getCampus();
+    this.getCursos();
   },
 
-
   methods: {
-    //método para preencher o data table
     async inicializar() {
       this.axios
         .get(`${baseApiUrl}api/usuario/search`)
         .then((res) => {
-          this.cursos = res.data.map((d) => ({ ...d, campus: d.campus.map((a) => a.label), }))
-          console.dir(res + "sucesuuu");
-          console.dir(this.cursos + "segura papaiiii");
+          this.usuarios = res.data.map((d) => ({
+            ...d,
+            campus: d.campus.map((a) => a.label), 
+            curso: d.curso.map((c) => c.label),
+          }));
+          console.log(this.usuarios + "array de usuários !!");
         })
         .catch((error) => {
-          console.warn(error);
+          console.log(error);
         });
-      await Promise.all([this.getCampus()]);
+      //await Promise.all([this.getCampus(), this.getCursos()]);
     },
-    //método para buscar campus existentes e preencher no array 
+
     async getCampus() {
-      const { data } = await this.axios.get(`${baseApiUrl}api/usuario/search`);
+      const { data } = await this.axios.get(`${baseApiUrl}api/campus/search`);
       this.campusRaw = data;
-      this.arraycampus = data;
-      //this.arraycampus = data.filter((d) => d.label);
-      console.log(`${this.arraycampus}array de campus aquii !!!!!!!!!!!`)
+      this.arraycampus = data.content;
+      console.log(this.arraycampus + " array de campus aqui !!");
+    },
+
+    async getCursos() {
+      const { data } = await this.axios.get(`${baseApiUrl}api/curso/search`);
+      this.cursosRaw = data;
+      this.arraycursos = data.content;
+      //console.log(this.arraycursos + "array de cursos aqui !!");
     },
 
     editItem(item) {
@@ -208,27 +277,22 @@ export default {
     },
 
     desativeItemConfirm() {
-      if (this.atributo.ativo == "Ativo") {
-        axios
-        patch(`${baseApiUrl}api/usuario/${this.atributo.id}`, {
-          ativo: false,
-        })
+      if (this.atributo.ativo) {
+        axios;
+        patch(`${baseApiUrl}api/usuario/${this.atributo.id}/${false}`)
           .then((res) => {
-            this.usuarios = res.data;
             console.log(res.data);
-            alert("Usuário desativado com sucesso!");
+            alert("Usuário desabilitado com sucesso!");
           })
           .catch((error) => {
             console.log(error);
           });
       } else {
         axios
-          .patch(`${baseApiUrl}api/usuario/${this.atributo.id}`, {
-            ativo: true,
-          })
+          .patch(`${baseApiUrl}api/usuario/${this.atributo.id}/${true}`)
           .then((res) => {
             console.log(res.data);
-            alert("Usuário ativado com sucesso !");
+            alert("Usuário habilitado com sucesso !");
           })
           .catch((error) => {
             console.log(error);
@@ -252,12 +316,41 @@ export default {
         this.editIndice = -1;
       });
     },
+    selecionarPerfil() {
+      switch (this.perfilSelecionado) {
+        case "Admin":
+          this.tipo = 1;
+          break;
+        case "Coordenador de Curso":
+          this.tipo = 2;
+          break;
+        case "Gestão Administrativa":
+          this.tipo = 3;
+          break;
+        default:
+          this.tipo = 0;
+      }
+      /* if (this.perfilSelecionado === "Admin") {
+         this.tipo = 1;
+      } else if (this.perfilSelecionado === "Coordenador de Curso") {
+        this.tipo = 2;
+      } else if (this.perfilSelecionado === "Gestão Administrativa") {
+        this.tipo = 3;
+      }else{
+         this.tipo = 0;
+      } 
+    */
+    },
 
     salvar() {
       if (this.editIndice > -1) {
         axios
           .put(`${baseApiUrl}api/usuario`, {
             id: this.atributo.id,
+            email: this.atributo.email,
+            tipo: this.atributo.tipo,
+            campus: this.atributo.campus,
+            curso: this.atributo.curso,
             ativo: this.atributo.ativo === "Ativo",
           })
           .then((res) => {
@@ -270,14 +363,20 @@ export default {
           });
         Object.assign(this.usuarios[this.editIndice], this.atributo);
       } else {
-        axios.post(`${baseApiUrl}api/usuario`, {
-          label: this.atributo.label,
-        }).then((res) => {
-          this.usuarios = res.data;
-          alert("Os dados foram adicionados com sucesso !");
-          console.log(res.data);
-          this.reloadPage();
-        });
+        axios
+          .post(`${baseApiUrl}api/usuario`, {
+            email: this.atributo.email,
+            tipo: this.atributo.tipo,
+            campus: this.atributo.campus,
+            curso: this.atributo.curso,
+            ativo: true,
+          })
+          .then((res) => {
+            this.usuarios = res.data;
+            alert("Os dados foram adicionados com sucesso !");
+            console.log(res.data);
+            this.reloadPage();
+          });
         this.usuarios.push(this.atributo);
       }
       this.fechar();
