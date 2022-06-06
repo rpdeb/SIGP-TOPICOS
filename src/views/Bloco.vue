76e115fb8de7a282fa57036008f088a30521e4fb
Lorenzo <63268260+alyne770@users.noreply.github.com>
2022-06-06 08:36:21 -0300

    Bloco.vue

--- src/views/Bloco.vue
+++ src/views/Bloco.vue
@@ -163,6 +163,10 @@ export default {
       dialog(val) {
         val || this.fechar();
       },
+
+      dialogDesativar(val) {
+      val || this.fecharDesativar();
+    },
     },
 
   mounted() {
