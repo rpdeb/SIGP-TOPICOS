4a322ff4f2eabab750bd99b991fc7ec073086d17
guilherme01 <lopesgui01@gmail.com>
2022-06-07 14:31:50 -0300

    atualizando rotas

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <template>
   <div id="app">
     <HomeMenu v-if="notIsLoginPage"/>
-    <router-view />
+     <v-main>
+      <router-view />
+    </v-main>
+    <v-footer color="#104ADF" >
+          <strong>SIGHA - Sistema de Gerenciamento de Horários Acadêmicos</strong>
+     </v-footer>
   </div>
 </template>
+
+
+<style scoped>
+.logo-unitins {
+  padding-top: 2.2%;
+  width: 80%;
+  height: 80%;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition-duration: 0.2s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter, .fade-leave {
+  opacity: 0;
+}
+</style>
 <script>
 import HomeMenu from "@/components/Menu";
 
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app id="inspire">
+
     <v-app-bar
       app
       color="#e7e7e7"
@@ -32,41 +32,9 @@
 
     </v-app-bar>
 
-    <v-main>
-      <router-view />
-    </v-main>
 
-   <v-footer color="#104ADF" padless>
-      <v-row justify="center" no-gutters>
-        <v-col class="py-4 text-center white--text" cols="12">
-          {{ new Date().getFullYear() }} —
-          <strong
-            >SIGHA - Sistema de Gerenciamento de Horários Acadêmicos</strong
-          >
-        </v-col>
-      </v-row>
-    </v-footer>
-
-  </v-app>
 </template>
 
-<style scoped>
-.logo-unitins {
-  padding-top: 2.2%;
-  width: 80%;
-  height: 80%;
-}
-
-.fade-enter-active, .fade-leave-active {
-  transition-duration: 0.2s;
-  transition-property: opacity;
-  transition-timing-function: ease;
-}
-
-.fade-enter, .fade-leave {
-  opacity: 0;
-}
-</style>
 
 <script>
 import router from '@/router/index'
