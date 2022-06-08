96462da867fcea9ceee6b9f99a2045298887de0b
Aldir Lyra <aldir_lyra@hotmail.com>
2022-06-07 21:58:15 -0300

    revert

--- src/App.vue
+++ src/App.vue
@@ -4,30 +4,9 @@
      <v-main>
       <router-view />
     </v-main>
-    <v-footer color="#104ADF" >
-          <strong>SIGHA - Sistema de Gerenciamento de Horários Acadêmicos</strong>
-     </v-footer>
   </div>
 </template>
 
-
-<style scoped>
-.logo-unitins {
-  padding-top: 2.2%;
-  width: 80%;
-  height: 80%;
-}
-
-.fade-enter-active, .fade-leave-active {
-  transition-duration: 0.2s;
-  transition-property: opacity;
-  transition-timing-function: ease;
-}
-
-.fade-enter, .fade-leave {
-  opacity: 0;
-}
-</style>
 <script>
 import HomeMenu from "@/components/Menu";
 
