d205f317758117879cdd719223e90ea5061e3c65
luana.macedo <luana.macedo@ilia.digital>
2022-06-27 03:44:38 -0300

    Ajustes em oferta

--- src/views/Oferta.vue
+++ src/views/Oferta.vue
@@ -1,21 +1,46 @@
 <template>
-  <v-data-table :headers="titulos" :items="ofertas" :search="search" class="elevation-2 data-table" :footer-props="{
-    'items-per-page-text': 'Itens por página',
-  }">
+  <v-data-table
+    :headers="titulos"
+    :items="ofertas"
+    :search="search"
+    class="elevation-2 data-table"
+    :footer-props="{
+      'items-per-page-text': 'Itens por página',
+    }"
+  >
     <template v-slot:top>
       <v-toolbar flat>
         <v-toolbar-title>Oferta de Disciplina</v-toolbar-title>
         <v-divider class="mx-4" inset vertical></v-divider>
 
-        <v-text-field v-model="search" append-icon="mdi-magnify" label="Pesquisar" single-line hide-details>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Pesquisar"
+          single-line
+          hide-details
+        >
         </v-text-field>
         <v-spacer></v-spacer>
-        <v-select @change="filtrarPorAtivos" v-model="filtroSelecionado" :items="filtros" item-text="Filtro"
-          item-value="label"></v-select>
+        <v-select
+          @change="filtrarPorAtivos"
+          v-model="filtroSelecionado"
+          :items="filtros"
+          item-text="Filtro"
+          item-value="label"
+        ></v-select>
         <v-spacer></v-spacer>
         <v-dialog v-model="dialog" max-width="800px">
           <template v-slot:activator="{ on, attrs }" class="template-add">
-            <v-btn small class="mx-2 add" fab dark color="green" v-bind="attrs" v-on="on">
+            <v-btn
+              small
+              class="mx-2 add"
+              fab
+              dark
+              color="green"
+              v-bind="attrs"
+              v-on="on"
+            >
               <v-icon dark> mdi-plus</v-icon>
             </v-btn>
           </template>
@@ -27,45 +52,101 @@
             <v-card-text>
               <v-form>
                 <v-row>
+                  <v-col cols="8" sm="5" md="5">
+                    <v-select
+                      v-model="atributo.matriz"
+                      label="Matriz"
+                      item-text="matriz"
+                      item-value="codMatriz"
+                      onchange="getMatriz(nome)"
+                      :items="arraymatriz"
+                    ></v-select>
+                  </v-col>
+                  <v-col cols="8" sm="5" md="5">
+                    <v-select
+                      v-model="atributo.disciplina"
+                      label="Disciplina"
+                      item-text="label"
+                      item-value="codigoMatriz"
+                      :items="arraydisciplinas"
+                    ></v-select>
+                  </v-col>
+                  <v-col cols="8" sm="5" md="5">
+                    <v-select
+                      v-model="atributo.codpesoa"
+                      label="Professor"
+                      item-text="nome"
+                      :items="arrayprofessores"
+                    ></v-select>
+                  </v-col>
 
                   <v-col cols="8" sm="5" md="5">
-                    <v-select v-model="atributo.matriz" label="Matriz" item-text="matriz" item-value="codMatriz"
-                      onchange="getMatriz(nome)" :items="arraymatriz"></v-select>
-                  </v-col>
-                  <v-col cols="8" sm="5" md="5">
-                    <v-select v-model="atributo.disciplina" label="Disciplina" item-text="label"
-                      item-value="codigoMatriz" :items="arraydisciplinas"></v-select>
-                  </v-col>
-                  <v-col cols="8" sm="5" md="5">
-                    <v-select v-model="atributo.codpesoa" label="Professor" item-text="nome" item-value="codPessoa"
-                      :items="arrayprofessores"></v-select>
+                    <v-select
+                      v-model="atributo.sala"
+                      label="Sala"
+                      item-text="label"
+                      item-value="id"
+                      :items="arraysalas"
+                    ></v-select>
                   </v-col>
                 </v-row>
+                <v-row>
+                  <v-col cols="4" sm="5" md="5">
+                    <v-select
+                      v-model="atributo.bloco"
+                      label="Bloco"
+                      item-text="label"
+                      item-value="id"
+                      :items="arraybloco"
+                    ></v-select>
+                  </v-col>
 
+                  <v-col cols="4" sm="5" md="5">
+                    <v-select
+                      v-model="atributo.semestre"
+                      label="Semestre"
+                      item-text="label"
+                      item-value="id"
+                      :items="arraysemestres"
+                    ></v-select>
+                  </v-col>
+                </v-row>
+                <br />
+                <br />
+                <h3>Cadastrar horario</h3>
+                <br />
                 <v-row>
                   <v-col cols="8" sm="5" md="5">
-                    <v-select v-model="diaSemana" label="Dia da Semana" item-text="diasDaSemana" item-value="id"
-                      :items="diasDaSemana" />
-                  </v-col>
-
-
-                  <v-col cols="8" sm="5" md="5">
-                    <v-select v-model="horarios" label="Horário" item-text="diasDaSemana" item-value="id"
-                      :items="arrayoptionshora" multiple />
-
-                    <v-btn small class="mx-2 add" fab dark color="green" v-bind="attrs" v-on="on"/>
-                  </v-col>
-                
-                  <v-col cols="8" sm="5" md="5" class="horario-style">
                     <v-select
-                      v-model="horarios"
-                      label="Horário"
+                      v-model="atributo.horario.diaSemana"
+                      label="Dia da Semana"
                       item-text="diasDaSemana"
                       item-value="id"
+                      :items="diasDaSemana"
+                    />
+                  </v-col>
+                  <v-col cols="4" sm="5" md="5">
+                    <v-select
+                      v-model="atributo.horario.turno"
+                      label="Turno"
+                      item-text="turno"
+                      item-value="turno"
+                      :items="turnos"
+                    ></v-select>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="8" sm="5" md="10" class="horario-style">
+                    <v-select
+                      v-model="desserts"
+                      label="Horário"
+                      item-text="text"
+                      item-value="text"
                       :items="arrayoptionshora"
                       multiple
                     />
-                    
+                  </v-col>
+                  <v-col>
                     <v-btn
                       small
                       class="mx-2 add"
@@ -74,57 +155,61 @@
                       color="green"
                       v-bind="attrs"
                       v-on="on"
+                      @click="add"
                     >
                       <v-icon dark> mdi-plus</v-icon>
                     </v-btn>
                   </v-col>
                 </v-row>
 
-                <v-row>
-                  <v-col cols="8" sm="5" md="5">
-                    <v-select v-model="atributo.sala" label="Sala" item-text="label" item-value="id"
-                      :items="arraysalas"></v-select>
-                  </v-col>
-
-                 <v-col cols="8" sm="5" md="5">
-                   <v-select
-                        v-model="atributo.turno"
-                        label="Turno"
-                        item-text="turno"
-                        item-value="turno"
-                        :items="turnos"
-                      ></v-select> 
-                </v-col>
-
-                  <v-col cols="8" sm="5" md="5">
-                    <v-select v-model="atributo.bloco" label="Bloco" item-text="label" item-value="id"
-                      :items="arraybloco"></v-select>
-                  </v-col>
-
-
-                  <v-col cols="8" sm="5" md="5">
-                    <v-select v-model="atributo.semestre" label="Semestre" item-text="label" item-value="id"
-                      :items="arraysemestres"></v-select>
-                  </v-col>
-                </v-row>
+                <v-simple-table>
+                  <template v-slot:default>
+                    <thead>
+                      <tr>
+                        <th class="text-left">Dia da semana</th>
+                        <th class="text-left">Horario</th>
+                        <th class="text-left">Turno</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>{{ atributo.horario.diaSemana }}</td>
+                        <td>{{ desserts }}</td>
+                        <td>{{ atributo.horario.turno }}</td>
+                      </tr>
+                    </tbody>
+                  </template>
+                </v-simple-table>
               </v-form>
             </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn small color="warning" dark @click="fechar">Cancelar</v-btn>
-              <v-btn small color="primary" class="mr-4" @click="salvar">Salvar</v-btn>
+              <v-btn small color="primary" class="mr-4" @click="salvar"
+                >Salvar</v-btn
+              >
             </v-card-actions>
           </v-card>
         </v-dialog>
 
         <v-dialog v-model="dialogDesativar" max-width="400px">
           <v-card>
-            <v-card-title class="text-h5">Deseja {{ mudarStatus }} este oferta ?</v-card-title>
+            <v-card-title class="text-h5"
+              >Deseja {{ mudarStatus }} este oferta ?</v-card-title
+            >
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn small color="warning" dark @click="dialogDesativar = false">
-                Não</v-btn>
-              <v-btn small color="primary" dark @click="desativeItemConfirm">Sim</v-btn>
+              <v-btn
+                small
+                color="warning"
+                dark
+                @click="dialogDesativar = false"
+              >
+                Não</v-btn
+              >
+              <v-btn small color="primary" dark @click="desativeItemConfirm"
+                >Sim</v-btn
+              >
               <v-spacer></v-spacer>
             </v-card-actions>
           </v-card>
@@ -170,12 +255,22 @@ export default {
       //{ text: "Status", value: "ativo" },
       { text: "Ações", value: "acoes" },
     ],
+    desserts: [],
+    desserts1: {
+      name: "oi",
+      calories: "oi",
+    },
     ofertas: [],
     salasRaw: [],
     arraybloco: [],
     arraysalas: [],
     horarios: [],
-    arrayoptionshora: ["horario1", "horario2", "horario3", "horario4"],
+    arrayoptionshora: [
+      { text: "horario 1", value: 1 },
+      { text: "horario 2", value: 2 },
+      { text: "horario 3", value: 3 },
+      { text: "horario 4", value: 4 },
+    ],
     arraysemestres: [],
     arraydisciplinas: [],
     arraymatriz: [],
@@ -187,14 +282,16 @@ export default {
     editIndice: -1,
     atributo: {
       id: null,
-      horario: {
-        horario1: false,
-        horario2: false,
-        horario3: false,
-        horario4: false,
-        turno: "",
-        diaSemana: "",
-      },
+      horario: [
+        {
+          horario1: false,
+          horario2: false,
+          horario3: false,
+          horario4: false,
+          turno: "",
+          diaSemana: "",
+        },
+      ],
       matriz: null,
       disciplina: null,
       sala: null,
@@ -315,7 +412,7 @@ export default {
       const { data } = await this.axios.get(
         `${baseApiUrl}api/disciplina/search`
       );
-      this.arraydisciplinas = data;
+      this.arraydisciplinas = data.content;
       console.log(this.arraydisciplinas + "array de disciplinas aqui");
     },
 
@@ -326,7 +423,9 @@ export default {
       this.arraymatriz = data;
       console.log(this.arraymatriz + "array de matriz aqui");
     },
-
+    add() {
+      console.log("desserts " + desserts);
+    },
     filtrarPorAtivos() {
       if (this.filtroSelecionado === "Todos") {
         // const json = localStorage.getItem(userKey);
@@ -335,18 +434,18 @@ export default {
         axios
           .get(`${baseApiUrl}api/oferta/search`)
           .then((res) => {
-            this.ofertas = res.data.content
-              .map((c) => {
-                c.ativo = c.ativo ? "Ativo" : "Inativo";
-                return c;
-              });
-            console.log("todos !!")
+            this.ofertas = res.data.content.map((c) => {
+              c.ativo = c.ativo ? "Ativo" : "Inativo";
+              return c;
+            });
+            console.log("todos !!");
             console.log(res.data);
-          }).catch((error) => {
+          })
+          .catch((error) => {
             console.log(error);
           });
       } else {
-        console.log("ativos !!")
+        console.log("ativos !!");
         this.inicializar();
       }
     },
@@ -390,6 +489,8 @@ export default {
       this.fecharDesativar();
     },
 
+    addHorario() {},
+
     reloadPage() {
       window.location.reload();
     },
@@ -431,14 +532,16 @@ export default {
       } else {
         axios
           .post(`${baseApiUrl}api/oferta`, {
-            horario: {
-              horario1: this.atributo.horario.horario1,
-              horario2: this.atributo.horario.horario2,
-              horario3: this.atributo.horario.horario3,
-              horario4: this.atributo.horario.horario4,
-              turno: this.atributo.horario.turno,
-              diaSemana: this.atributo.horario.diaSemana,
-            },
+            horario: [
+              {
+                horario1: this.atributo.horario.horario1,
+                horario2: this.atributo.horario.horario2,
+                horario3: this.atributo.horario.horario3,
+                horario4: this.atributo.horario.horario4,
+                turno: this.atributo.horario.turno,
+                diaSemana: this.atributo.horario.diaSemana,
+              },
+            ],
             disciplina: this.atributo.disciplina,
             sala: this.atributo.sala,
             bloco: this.atributo.bloco,
